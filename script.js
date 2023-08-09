854950cad8b74d2e5b9beb5bb0829b5def312b80
Prisca <70857042+PriscaOnyee@users.noreply.github.com>
2023-08-09 23:04:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
 
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+
+
+function updateCalendar() {
+
+    const today = new Date();
+
+    date.innerHTML = today.getDate();
+    day.innerHTML = daysOfWeek[today.getDay()];
+    month.innerHTML = months[today.getMonth()];
+    year.innerHTML = today.getFullYear();
+
+    console.log(today);
+}
+
+updateCalendar();
+
+
+
+function updateTime(){
+    const currentTime = new Date();
+
+    const hours = String(currentTime.getHours()).padStart(2, '0');
+    const minutes = String(currentTime.getMinutes()).padStart(2, '0');
+    const seconds = String(currentTime.getSeconds()).padStart(2, '0');
+
+    const displayTime = `${hours} : ${minutes} : ${seconds}`;
+
+    const digitalTime = document.getElementById("time");
+    digitalTime.innerHTML = displayTime;
+
+    console.log(currentTime);
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
